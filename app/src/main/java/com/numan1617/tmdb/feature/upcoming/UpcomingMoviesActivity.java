83a663cc3d782554d9f2013dcab2618603d2c96d
James Newman <numan1617@gmail.com>
2015-07-20 21:09:24 +0100

    Added upcoming movies activity

---
+++ app/src/main/java/com/numan1617/tmdb/feature/upcoming/UpcomingMoviesActivity.java
@@ -0,0 +1,18 @@
+package com.numan1617.tmdb.feature.upcoming;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.numan1617.tmdb.R;
+
+/**
+ * Created by jamesnewman on 20/07/15.
+ */
+public class UpcomingMoviesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upcoming_movies);
+    }
+}
