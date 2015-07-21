19b74f4219ec4ed4e7dedf45b30858f1a19ea0b4
James Newman <numan1617@gmail.com>
2015-07-21 21:51:11 +0100

    Added getting of upcoming movies

--- app/src/main/java/com/numan1617/tmdb/feature/upcoming/UpcomingMoviesActivity.java
+++ app/src/main/java/com/numan1617/tmdb/feature/upcoming/UpcomingMoviesActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.numan1617.api.MovieService;
+import com.numan1617.api.model.Movie;
+import com.numan1617.api.model.PagedResults;
 import com.numan1617.tmdb.R;
 import com.numan1617.tmdb.TmdbApplicationComponent;
 import com.numan1617.tmdb.base.BaseActivity;
@@ -13,11 +16,16 @@ import javax.inject.Inject;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import rx.Observer;
+import rx.subscriptions.CompositeSubscription;
+import timber.log.Timber;
 
 /**
  * Created by jamesnewman on 20/07/15.
  */
 public class UpcomingMoviesActivity extends BaseActivity {
+
+    private CompositeSubscription compositeSubscription = new CompositeSubscription();
 
     @Bind(R.id.recycler_movie_list)
     RecyclerView movieList;
@@ -25,12 +33,23 @@ public class UpcomingMoviesActivity extends BaseActivity {
     @Inject
     UpcomingMovieListAdapter movieListAdapter;
 
+    @Inject
+    MovieService movieService;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ButterKnife.bind(this);
 
         setupMovieList();
+        getUpcomingMoviesForPage(1);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        compositeSubscription.unsubscribe();
     }
 
     private void setupMovieList() {
@@ -51,5 +70,26 @@ public class UpcomingMoviesActivity extends BaseActivity {
     @Override
     protected int layoutId() {
         return R.layout.activity_upcoming_movies;
+    }
+
+    private void getUpcomingMoviesForPage(int page) {
+        compositeSubscription.add(movieService.getUpcomingMovies(page, "en")
+                        .subscribe(new Observer<PagedResults<Movie>>() {
+                            @Override
+                            public void onCompleted() {
+
+                            }
+
+                            @Override
+                            public void onError(Throwable e) {
+                                Timber.e(e, "movieService.getUpcomingMovies onError");
+                            }
+
+                            @Override
+                            public void onNext(PagedResults<Movie> moviePagedResults) {
+                                movieListAdapter.setMovies(moviePagedResults.getResults());
+                            }
+                        })
+        );
     }
 }
