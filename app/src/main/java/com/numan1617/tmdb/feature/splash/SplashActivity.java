25511ac90a09e1d9d82daea6319efe78382ac06d
James Newman <numan1617@gmail.com>
2016-01-16 00:26:30 +0000

    Splash creation

---
+++ app/src/main/java/com/numan1617/tmdb/feature/splash/SplashActivity.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2016 James Newman (Numan1617)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.numan1617.tmdb.feature.splash;
+
+import com.numan1617.tmdb.R;
+import com.numan1617.tmdb.TmdbApplicationComponent;
+import com.numan1617.tmdb.base.BaseActivity;
+
+public class SplashActivity extends BaseActivity implements SplashPresenter.View {
+
+    @Override
+    protected void onCreateComponent(TmdbApplicationComponent applicationComponent) {
+        DaggerSplashComponent.builder()
+                .tmdbApplicationComponent(applicationComponent)
+                .splashModule(new SplashModule(this))
+                .build()
+                .inject(this);
+    }
+
+    @Override
+    protected int layoutId() {
+        return R.layout.activity_splash;
+    }
+}
