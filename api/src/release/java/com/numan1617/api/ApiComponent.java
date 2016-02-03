87f1530896e1b4037705d881bf83bd47d5f36977
James Newman <numan1617@gmail.com>
2016-02-03 23:20:00 +0000

    Added config service to release module

--- api/src/release/java/com/numan1617/api/ApiComponent.java
+++ api/src/release/java/com/numan1617/api/ApiComponent.java
@@ -11,4 +11,5 @@ import dagger.Component;
 @Component(modules = { ReleaseApiModule.class })
 public interface ApiComponent {
     MovieService movieService();
+    ConfigurationService configurationService();
 }
